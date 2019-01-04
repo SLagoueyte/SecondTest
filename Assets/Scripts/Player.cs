e6cff61d87a3474f5a3dcd7d4c1be7a60796e19e
chgutierrez <chgutierrez@timba.co>
2019-01-04 12:11:24 -0500

    Adding the tutorial

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    Rigidbody rigidbody;
+    Vector3 velocity;
+
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+        velocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized * 10;	
+	}
+
+    private void FixedUpdate() {
+        rigidbody.MovePosition(rigidbody.position + velocity * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Scripts/Player2D.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2D : MonoBehaviour {
+
+    Rigidbody2D rigidbody;
+    Vector2 velocity;
+
+	void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+        velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * 10;	
+	}
+
+    private void FixedUpdate() {
+        rigidbody.MovePosition(rigidbody.position + velocity * Time.fixedDeltaTime);
+    }
+}
